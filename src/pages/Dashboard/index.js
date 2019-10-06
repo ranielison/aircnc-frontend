08bd896b969f0c3fbe0f1a40d3cc289bac010e09
Ranielison <ranielisonsoares2@gmail.com>
2019-10-06 15:15:55 -0300

    finalizando a aplicaçao web

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import api from "../../services/api";
 
+import "./styles.css";
+
 export default function Dashboard() {
+  const [spots, setSpots] = useState([]);
+
   useEffect(() => {
     async function loadSpots() {
       const user_id = localStorage.getItem("user");
@@ -10,10 +15,32 @@ export default function Dashboard() {
           user_id
         }
       });
+      console.log(response.data);
+      setSpots(response.data);
     }
 
     loadSpots();
   }, []);
 
-  return <div />;
+  return (
+    <>
+      <ul className="spot-list">
+        {spots.map(spot => (
+          <li key={spot._id}>
+            <header
+              style={{
+                backgroundImage: `url(${spot.thumbnail_url})`,
+                backgroundSize: "cover"
+              }}
+            ></header>
+            <strong>{spot.company}</strong>
+            <span>{spot.price ? `R$${spot.price}/dia` : "Gratuito"}</span>
+          </li>
+        ))}
+      </ul>
+      <Link to="/new">
+        <button className="btn">Cadastrar novo spot</button>
+      </Link>
+    </>
+  );
 }
